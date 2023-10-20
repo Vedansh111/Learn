d3e9cfd01b5feaf79c3f4f8307c780daeccde537
Vedansh111 <123576150+Vedansh111@users.noreply.github.com>
2023-10-20 12:45:21 +0530

    Init

---
+++ Learn/Ch 1 - Inception/script.js
@@ -0,0 +1,13 @@
+const h1 = React.createElement('h1', {}, 'Waah Vedansh')
+const h2 = React.createElement('h2', {}, 'Waah Waah Vedansh')
+const a = React.createElement('h1', {}, "okiee")
+const comb = React.createElement('div', {
+    id: "Nope",
+    style: { color: 'purple', background: 'lightpink' },
+    nothing: "dumb"
+}, [h1, h2, a])
+
+
+const root = ReactDOM.createRoot(document.getElementById('waah'))
+
+root.render(comb)
